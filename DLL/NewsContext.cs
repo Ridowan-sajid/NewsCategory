66a2ea8c8c96a305d624f1f7eb39a3c860749211
Ridowan-sajid <sajidridowan7@gmail.com>
2023-04-01 23:46:05 +0600

    Add project files.

---
+++ DLL/NewsContext.cs
@@ -0,0 +1,16 @@
+﻿using DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    class NewsContext:DbContext
+    {
+        public DbSet<News> Newses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ DLL/Repos/NewsRepo.cs
@@ -0,0 +1,28 @@
+﻿using DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repos
+{
+    public class NewsRepo
+    {
+        static NewsContext newsContext;
+        static NewsRepo()
+        {
+            newsContext = new NewsContext();
+        }
+        public static List<News> Get()
+        {
+            return newsContext.Newses.ToList();
+        }
+        public static News Get(int id)
+        {
+            return newsContext.Newses.Find(id);
+        }
+
+
+    }
+}
